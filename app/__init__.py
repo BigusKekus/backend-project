15adda8918472e474ebeda29b1c7deb462c2e529
BigusKekus <misha.vihlaev@gmail.com>
2025-12-19 21:15:30 +0300

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,11 +1,10 @@
 from flask import Flask
 
 
-def create_app() -> Flask:
+def create_app():
     app = Flask(__name__)
 
     from app.views import register_routes
 
     register_routes(app)
-
     return app
